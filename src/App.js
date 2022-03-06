b4b6fddc6cc39bb8582a01e46bfb4405f599d547
jpsm83 <73789298+jpsm83@users.noreply.github.com>
2022-03-06 11:57:31 +0100

    geocodeLocation

--- src/App.js
+++ src/App.js
@@ -8,22 +8,45 @@ import { Route, Routes } from "react-router-dom"
 function App() {
   const [weather, setWeather] = useState();
 
+  // useEffect(() => {
+  //   const API_KEY = "9b6ef062a1de6aab80c70f4044a9bdc7";
+  //   navigator.geolocation.getCurrentPosition((success) => {
+  //     const { latitude, longitude } = success.coords;
+  //     axios
+  //       .get(
+  //         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+  //       )
+  //       .then((res) => {
+  //         setWeather(res.data);
+  //         console.log(success);
+  //       })
+  //       .catch((error) => console.log(error));
+  //   });
+  // }, []);
+
+  // console.log(weather);
+
   useEffect(() => {
-    const API_KEY = "9b6ef062a1de6aab80c70f4044a9bdc7";
-    navigator.geolocation.getCurrentPosition((success) => {
-      const { latitude, longitude } = success.coords;
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
-        )
-        .then((res) => {
-          setWeather(res.data);
-        })
-        .catch((error) => console.log(error));
-    });
+    geolocation()
   }, []);
 
-  console.log(weather);
+  const geolocation = () => {
+    const location = "carrer mallorca 585 barcelona spain 08026"
+    const googleApiKey = "AIzaSyDvgGZ6g32NvcVVPHR8rWyXc_syAkGQEvE";
+    // const mapsURL = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${googleApiKey}`;
+
+    const mapsURL = `https://maps.googleapis.com/maps/api/geocode/json`;
+    axios.get(mapsURL, {
+      params: {
+        address: location,
+        key: googleApiKey
+      }
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch(error => console.log(error))
+  }
 
   return (
     <div>
